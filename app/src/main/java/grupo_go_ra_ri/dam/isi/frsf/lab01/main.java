1552769da043de6f769024bae88c41aa06f77063
hrickert <hernan_hjr@hotmail.com>
2017-08-30 22:37:09 -0300

    Se agrega CHECKLIST.md con todos lo que falta

--- app/src/main/java/grupo_go_ra_ri/dam/isi/frsf/lab01/main.java
+++ app/src/main/java/grupo_go_ra_ri/dam/isi/frsf/lab01/main.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class main extends AppCompatActivity {
@@ -15,18 +16,35 @@ public class main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn_do = (Button)findViewById(R.id.id_btn_do);
-
         btn_do.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 TextView txt_response = (TextView)findViewById(R.id.id_txt_response);
+
+                if (1==1) {
+                    txt_response.setText("Plazo fijo realizado. Recibirá 5000 al vencimiento.");
+                    txt_response.setTextColor(getResources().getColor(R.color.ok));
+                }
+
                 //txt_response.setVisibility(View.VISIBLE);
                 //txt_response.setTextColor(getResources().getColor(R.color.ok));
-
-                //if (1==0) {
-                //    txt_response.setText("Plazo fijo realizado. Recibirá 5000 al vencimiento.");
-                //    txt_response.setTextColor(getResources().getColor(R.color.ok));
-                //}
             }
+        });
+
+        SeekBar inversion_days = (SeekBar) findViewById(R.id.id_inversion_days);
+        inversion_days.setMax(360);
+        inversion_days.setProgress(1);
+        inversion_days.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                TextView sel_days = (TextView)findViewById(R.id.id_select_days);
+                sel_days.setText((char) i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
     }
 }
